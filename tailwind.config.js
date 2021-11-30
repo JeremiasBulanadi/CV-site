f57dc8c9ecaf9716d1ff71a376f1b4a26b19d315
Jeremias Bulanadi <jdb.prog@gmail.com>
2021-11-30 12:46:51 +0800

    Tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const { plugins } = require("./postcss.config");
+
+const production = !process.env.ROLLUP_WATCH;
+module.exports = {
+    future: {
+        purgeLayersByDefault: true,
+        removeDeprecatedGapUtilities: true,
+    },
+    plugins: [
+    ],
+    purge: {
+        content: [
+            './src/App.svelte',
+        ],
+        enabled: production // disable purge in dev
+    }
+};
