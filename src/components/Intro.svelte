f3d243fe4fae558b18df8ec25592a8b745c22ead
Jeremias Bulanadi <jdb.prog@gmail.com>
2021-12-05 02:50:26 +0800

    Test commit

--- src/components/Intro.svelte
+++ src/components/Intro.svelte
@@ -1,18 +1,20 @@
 <main>
     <div class="container flex justify-center">
-        <h3>
+        <h3 class="text">
             Hello there, I'm
         </h3>
-        <h1>
+        <h1 class="text">
             Jeremias Bulanadi.
         </h1>
-        <h2>
+        <h2 class="text">
             I like making stuff.
         </h2>
-        <a href="./JeremiasBulanadi-Resume.pdf" target="_blank">Get Resume</a>
+        <a href="./JeremiasBulanadi-Resume.pdf" target="_blank" class="text">Get Resume</a>
     </div>
 </main>
 
 <style>
-    
+    .text {
+        color: white;
+    }
 </style>
