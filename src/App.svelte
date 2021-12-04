5f1c92e01df33efd6ba7a659503912c5f71d7162
Jeremias Bulanadi <jdb.prog@gmail.com>
2021-12-05 02:30:28 +0800

    Test commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,21 @@
 <script>
+	import Intro from './components/Intro.svelte';
+
 	export let name;
 </script>
 
-<main>
-	<div ></div>
+<main class="bg-gray-800">
+	<Intro>
+	</Intro>
 </main>
 
 <style>
+
+	main {
+		width: 100vw;
+		height: 100vh;
+		padding: 8px;
+	}
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
---
+++ src/components/Intro.svelte
@@ -0,0 +1,18 @@
+<main>
+    <div class="container flex justify-center">
+        <h3>
+            Hello there, I'm
+        </h3>
+        <h1>
+            Jeremias Bulanadi.
+        </h1>
+        <h2>
+            I like making stuff.
+        </h2>
+        <a href="./JeremiasBulanadi-Resume.pdf" target="_blank">Get Resume</a>
+    </div>
+</main>
+
+<style>
+    
+</style>
