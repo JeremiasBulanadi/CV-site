8a6a6b0aee57385b270dc32fb8657921a7eaa557
Jeremias Bulanadi <jdb.prog@gmail.com>
2021-12-06 01:25:02 +0800

    Successful operations

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+
+</script>
+
+<main class="wrapper bg-gray-900">
+	<div class="py-8 px-4 bg-red-400 border border-indigo-500 shadow-lg">
+		this is a card
+	</div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 2em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	.wrapper {
+		width: 100vw;
+		height: 100vh;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
